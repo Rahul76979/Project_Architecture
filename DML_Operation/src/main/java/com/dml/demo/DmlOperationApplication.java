fc00117ae2b389c4a1b04a998b7c8a7f123de0f7
Rahul <rahulsirwani181989@gmail.com>
2020-11-15 12:16:37 +0530

    This is my First commit.

---
+++ DML_Operation/src/main/java/com/dml/demo/DmlOperationApplication.java
@@ -0,0 +1,13 @@
+package com.dml.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DmlOperationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DmlOperationApplication.class, args);
+	}
+
+}
---
+++ DML_Operation/src/test/java/com/dml/demo/DmlOperationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dml.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DmlOperationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
